fca1708c35e6bc56fccc3395ca309771a3bdef97
EmulationNerds685 <bhaskartiwari270@gmail.com>
2024-09-26 13:32:07 +0530

    Improved frontend

--- index.js
+++ index.js
@@ -13,46 +13,34 @@ app.get('/', async (req, res) => {
 	res.render('index.ejs')
 })
 app.post('/submit', async (req, res) => {
-	const food_item = req.body.food
-	const options = {
-		url: 'https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition',
-		params: {
-			query: food_item
-		},
-		headers: {
-			'X-RapidAPI-Key': '6b1765fc18mshdf2d0e8b1d5de67p1ac4b4jsn2944a1d76ed3',
-			'X-RapidAPI-Host': 'nutrition-by-api-ninjas.p.rapidapi.com'
-		}
-	}
+	
 
+	const options = {
+	  method: 'GET',
+	  url: 'https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition',
+	  params: {
+		query: req.body.food
+	  },
+	  headers: {
+		'x-rapidapi-key': '6b1765fc18mshdf2d0e8b1d5de67p1ac4b4jsn2944a1d76ed3',
+		'x-rapidapi-host': 'nutrition-by-api-ninjas.p.rapidapi.com'
+	  }
+	};
+	
 	try {
-		const response = await axios.get(options.url, {
-			params: options.params,
-			headers: options.headers
-		})
-		const content = response.data[0]
-		const len = response.data
-		if (len.length == 1) {
-			res.render('index.ejs', {
-				name: req.body.food,
-				info: content
-			})
-		} else {
-			console.log('error')
-			res.render('index.ejs', {
-				name: null,
-				info: null,
-				error: error.message
-			})
+		const response = await axios.request(options);
+		console.log(response.data)
+		console.log(response.data.length)
+		const result=response.data		
+		if((result).length==0){
+			res.render('index.ejs',{error:"Invalid Food Item!"})
+			console.log("Not Found!")
 		}
-		/**/
-	} catch (error) {
-		console.log('Error')
-		res.render('index.ejs', {
-			name: null,
-			info: null,
-			error: error.message
-		})
+		else{
+			res.render("index.ejs",{name:req.body.food,info:result[0]})
+		}
+	} catch (error){
+	console.error(error);
 	}
 })
 
